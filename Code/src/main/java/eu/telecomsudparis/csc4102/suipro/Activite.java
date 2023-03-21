adf5a82030bec85140e710f5baca340a6de4abd4
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-03-21 10:53:14 +0100

    Fix typo

--- Code/src/main/java/eu/telecomsudparis/csc4102/suipro/Activite.java
+++ Code/src/main/java/eu/telecomsudparis/csc4102/suipro/Activite.java
@@ -23,8 +23,6 @@ public class Activite extends ElementJetable implements IActivite {
         this.id = id;
         this.taches = new LinkedHashMap<>();
 
-        this.taches = new ArrayList<Tache>();
-
         assert invariant();
     }
 
