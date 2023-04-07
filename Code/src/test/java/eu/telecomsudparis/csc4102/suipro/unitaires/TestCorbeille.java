91128b28990c97b3e26977a82b31e44c2eee9470
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-04-07 15:09:59 +0200

    Update unit test for  Corbeille

--- Code/src/test/java/eu/telecomsudparis/csc4102/suipro/unitaires/TestCorbeille.java
+++ Code/src/test/java/eu/telecomsudparis/csc4102/suipro/unitaires/TestCorbeille.java
@@ -1,7 +1,12 @@
+// CHECKSTYLE:OFF
 package eu.telecomsudparis.csc4102.suipro.unitaires;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -12,22 +17,21 @@ import eu.telecomsudparis.csc4102.suipro.ITache;
 import eu.telecomsudparis.csc4102.suipro.mocks.MockedActivite;
 import eu.telecomsudparis.csc4102.suipro.mocks.MockedDeveloppeur;
 import eu.telecomsudparis.csc4102.suipro.mocks.MockedPeriodeDeTravail;
+import eu.telecomsudparis.csc4102.suipro.mocks.MockedElementJetable;
 import eu.telecomsudparis.csc4102.suipro.mocks.MockedTache;
+import eu.telecomsudparis.csc4102.util.OperationImpossible;
 
 public class TestCorbeille {
-    @AfterEach
-    void tearDown() throws Exception {
-        Corbeille.getInstance().viderLaCorbeille();
+    Corbeille corbeille;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        corbeille = new Corbeille();
     }
 
-    @Nested
-    class GetInstance {
-
-        @Test
-        void Test1() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
-            Assertions.assertNotNull(corbeille);
-        }
+    @AfterEach
+    void tearDown() throws Exception {
+        corbeille = null;
     }
 
     @Nested
@@ -35,16 +39,16 @@ public class TestCorbeille {
 
         @Test
         void Test1() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
-            Assertions.assertThrows(IllegalArgumentException.class, () -> corbeille.ajouterALaCorbeille(null));
+            Assertions.assertThrows(OperationImpossible.class, () -> corbeille.ajouterALaCorbeille(null));
         }
 
         @Test
         void Test2() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
-            MockedDeveloppeur developpeur = new MockedDeveloppeur(true, false);
-            corbeille.ajouterALaCorbeille(developpeur);
-            Assertions.assertEquals(1, corbeille.getElementsJetable(MockedDeveloppeur.class).size());
+            MockedElementJetable elem = new MockedElementJetable();
+            corbeille.ajouterALaCorbeille(elem);
+            Assertions.assertEquals(1, corbeille.getElementsJetable(MockedElementJetable.class).size());
+            corbeille.ajouterALaCorbeille(elem);
+            Assertions.assertEquals(1, corbeille.getElementsJetable(MockedElementJetable.class).size());
         }
     }
 
@@ -53,33 +57,30 @@ public class TestCorbeille {
 
         @Test
         void Test1() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
-            Assertions.assertThrows(IllegalArgumentException.class, () -> corbeille.supprimerDeLaCorbeille(null));
+            Assertions.assertThrows(OperationImpossible.class, () -> corbeille.supprimerDeLaCorbeille(null));
         }
 
         @Test
         void Test2() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
-            MockedDeveloppeur developpeur = new MockedDeveloppeur(true, false);
-            corbeille.ajouterALaCorbeille(developpeur);
-            Assertions.assertEquals(1, corbeille.getElementsJetable(MockedDeveloppeur.class).size());
-            corbeille.supprimerDeLaCorbeille(developpeur);
-            Assertions.assertEquals(0, corbeille.getElementsJetable(MockedDeveloppeur.class).size());
+            MockedElementJetable elem = new MockedElementJetable();
+            corbeille.ajouterALaCorbeille(elem);
+            Assertions.assertEquals(1, corbeille.getElementsJetable(MockedElementJetable.class).size());
+            corbeille.supprimerDeLaCorbeille(elem);
+            Assertions.assertEquals(0, corbeille.getElementsJetable(MockedElementJetable.class).size());
+            corbeille.supprimerDeLaCorbeille(elem);
+            Assertions.assertEquals(0, corbeille.getElementsJetable(MockedElementJetable.class).size());
         }
     }
 
     @Nested
     class GetElementsJetable {
-
         @Test
         void Test1() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
-            Assertions.assertThrows(IllegalArgumentException.class, () -> corbeille.getElementsJetable(null));
+            Assertions.assertThrows(OperationImpossible.class, () -> corbeille.getElementsJetable(null));
         }
 
         @Test
         void Test2Jeu1() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
             MockedDeveloppeur developpeur = new MockedDeveloppeur(true);
             corbeille.ajouterALaCorbeille(developpeur);
             Assertions.assertEquals(1, corbeille.getElementsJetable(IDeveloppeur.class).size());
@@ -87,7 +88,6 @@ public class TestCorbeille {
 
         @Test
         void Test2Jeu2() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
             MockedActivite activite = new MockedActivite(true);
             corbeille.ajouterALaCorbeille(activite);
             Assertions.assertEquals(1, corbeille.getElementsJetable(IActivite.class).size());
@@ -95,7 +95,6 @@ public class TestCorbeille {
 
         @Test
         void Test2Jeu3() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
             MockedTache tache = new MockedTache(true);
             corbeille.ajouterALaCorbeille(tache);
             Assertions.assertEquals(1, corbeille.getElementsJetable(ITache.class).size());
@@ -103,10 +102,37 @@ public class TestCorbeille {
 
         @Test
         void Test2Jeu4() throws Exception {
-            Corbeille corbeille = Corbeille.getInstance();
             MockedPeriodeDeTravail periodeDeTravail = new MockedPeriodeDeTravail(true);
             corbeille.ajouterALaCorbeille(periodeDeTravail);
             Assertions.assertEquals(1, corbeille.getElementsJetable(MockedPeriodeDeTravail.class).size());
+        }
+    }
+
+    @Nested
+    class ViderLaCorbeille {
+        PropertyChangeListener listener;
+        PropertyChangeEvent evt;
+
+        @BeforeEach
+        void setUp() throws Exception {
+            listener = (evt) -> {
+                this.evt = evt;
+            };
+            corbeille.addPropertyChangeListener(listener);
+        }
+
+        @Test
+        void Test1() throws Exception {
+            MockedElementJetable elem = new MockedElementJetable();
+            corbeille.ajouterALaCorbeille(elem);
+            Assertions.assertEquals(1, corbeille.getElementsJetable(MockedElementJetable.class).size());
+            corbeille.viderLaCorbeille();
+            Assertions.assertEquals(0, corbeille.getElementsJetable(MockedElementJetable.class).size());
+
+            Assertions.assertEquals(corbeille, evt.getSource());
+            Assertions.assertEquals(elem.getClass().getSimpleName(), evt.getPropertyName());
+            Assertions.assertNull(evt.getNewValue());
+            Assertions.assertEquals(elem, evt.getOldValue());
         }
     }
 }
