d7a185d713ec097353c92cfc2d73c4a57878203f
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-04-07 15:06:26 +0200

    Delete singleton from Corbeille and add PCS

--- Code/src/main/java/eu/telecomsudparis/csc4102/suipro/Corbeille.java
+++ Code/src/main/java/eu/telecomsudparis/csc4102/suipro/Corbeille.java
@@ -1,89 +1,102 @@
 package eu.telecomsudparis.csc4102.suipro;
 
-import java.util.ArrayList;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.LinkedHashSet;
-import java.util.stream.Collectors;
+import java.util.List;
+
+import eu.telecomsudparis.csc4102.util.OperationImpossible;
 
 /**
- * Cette classe réalise le concept de corbeille. Une corbeille est un singleton
- * référençant une collection d'éléments jetables.
+ * Cette classe réalise le concept de corbeille. Une corbeille est un ensemble
+ * d'éléments jetables.
  */
-public final class Corbeille {
-    /**
-     * instance de la corbeille.
-     */
-    private static Corbeille instance;
-
+public final class Corbeille implements ICorbeille {
     /**
      * éléments jetables de la corbeille.
      */
-    private LinkedHashSet<IElementJetable> elementsJetable;
-
-    private Corbeille() {
-        this.elementsJetable = new LinkedHashSet<>();
-        assert invariant();
-    }
+    private final LinkedHashSet<IElementJetable> elementsJetable;
 
     /**
-     * @return l'instance de la corbeille
+     * support de gestion des écouteurs de propriétés.
      */
-    public static Corbeille getInstance() {
-        if (instance == null) {
-            instance = new Corbeille();
-        }
-        return instance;
+    private final PropertyChangeSupport support;
+
+    /**
+     * Constructeur.
+     */
+    public Corbeille() {
+        this.elementsJetable = new LinkedHashSet<>();
+        this.support = new PropertyChangeSupport(this);
+        assert invariant();
     }
 
     /**
      * @return true si l'invariant est respecté, false sinon
      */
     private boolean invariant() {
-        return this.elementsJetable != null;
+        return this.elementsJetable != null && this.support != null;
     }
 
     /**
      * @param elementJetable
-     * @throws IllegalArgumentException si l'élément jetable est null
+     * @throws OperationImpossible si l'élément jetable est null
      */
-    public void ajouterALaCorbeille(final IElementJetable elementJetable) throws IllegalArgumentException {
+    public void ajouterALaCorbeille(final IElementJetable elementJetable) throws OperationImpossible {
         if (elementJetable == null) {
-            throw new IllegalArgumentException("L'élément jetable ne peut pas être null.");
+            throw new OperationImpossible("L'élément jetable ne peut pas être null.");
         }
         this.elementsJetable.add(elementJetable);
+        assert invariant();
     }
 
     /**
      * @param elementJetable
-     * @throws IllegalArgumentException si l'élément jetable est null
+     * @throws OperationImpossible si l'élément jetable est null
      */
-    public void supprimerDeLaCorbeille(final IElementJetable elementJetable) throws IllegalArgumentException {
+    public void supprimerDeLaCorbeille(final IElementJetable elementJetable) throws OperationImpossible {
         if (elementJetable == null) {
-            throw new IllegalArgumentException("L'élément jetable ne peut pas être null.");
+            throw new OperationImpossible("L'élément jetable ne peut pas être null.");
         }
         this.elementsJetable.remove(elementJetable);
+        assert invariant();
     }
 
     /**
      * @param type le type des éléments jetables à récupérer
      * @param <T> le type des éléments jetables à récupérer
      * @return la liste des éléments jetables de la corbeille du type spécifié
-     * @throws IllegalArgumentException si le type est null
+     * @throws OperationImpossible si le type est null
      */
-    public <T extends IElementJetable> ArrayList<T> getElementsJetable(final Class<T> type) {
+    public <T extends IElementJetable> List<T> getElementsJetable(final Class<T> type) throws OperationImpossible {
         if (type == null) {
-            throw new IllegalArgumentException("Le type ne peut pas être null.");
+            throw new OperationImpossible("Le type ne peut pas être null.");
         }
 
         return this.elementsJetable.stream()
                 .filter(elementJetable -> type.isInstance(elementJetable))
                 .map(elementJetable -> type.cast(elementJetable))
-                .collect(Collectors.toCollection(ArrayList::new));
+                .toList();
     }
 
     /**
      * Vide la corbeille.
      */
     public void viderLaCorbeille() {
+        for (final IElementJetable elementJetable : this.elementsJetable) {
+            String nom = elementJetable.getClass().getSimpleName();
+            this.support.firePropertyChange(nom, elementJetable, null);
+            this.support.removePropertyChangeListener(elementJetable);
+        }
         this.elementsJetable.clear();
+        assert invariant();
+    }
+
+    /**
+     * @param listener
+     */
+    public void addPropertyChangeListener(final PropertyChangeListener listener) {
+        this.support.addPropertyChangeListener(listener);
+        assert invariant();
     }
 }
